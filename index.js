81fadc220d63a1f4e79fe30be95d338d57152626
johir-rayhan <johir.rayhan01@gmail.com>
2021-12-30 15:50:14 +0600

    Add UI source

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+// UI Source
+// https://projects.invisionapp.com/share/4711QOIQSMBG#/screens/458568554_register?browse
