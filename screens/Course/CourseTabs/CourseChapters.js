519bf849a4b3cec617e995928ce959c12545519a
johir-rayhan <johir.rayhan01@gmail.com>
2022-01-20 11:44:45 +0600

    WIP CourseDetails..chapters list

--- screens/Course/CourseTabs/CourseChapters.js
+++ screens/Course/CourseTabs/CourseChapters.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import {View, Text, ScrollView, Image, FlatList} from 'react-native';
 
@@ -20,13 +21,14 @@ const CourseChapters = ({title}) => {
         style={{
           marginTop: SIZES.padding,
           paddingHorizontal: SIZES.padding,
-        }}
-      >
+        }}>
         {/* Title */}
         <Text style={{...FONTS.h2}}>{dummyData.course_details.title}</Text>
         {/* Students & durations */}
         <View style={{flexDirection: 'row', marginTop: SIZES.base}}>
-          <Text style={{color: COLORS.gray30, ...FONTS.body4}}>{dummyData.course_details.number_of_students} </Text>
+          <Text style={{color: COLORS.gray30, ...FONTS.body4}}>
+            {dummyData.course_details.number_of_students}{' '}
+          </Text>
           <IconLabel
             icon={icons.time}
             label={dummyData.course_details.duration}
@@ -37,41 +39,39 @@ const CourseChapters = ({title}) => {
               ...FONTS.body4,
             }}
             iconStyle={iconSize(15)}
-            />
+          />
         </View>
         {/* Instructor */}
-        <View
-          style={[styles.row, {marginTop: SIZES.radius}]}
-        >
+        <View style={[styles.row, {marginTop: SIZES.radius}]}>
           {/* Profile */}
-            <Image
-              source={images.profile}
-              style={{
-                ...iconSize(50),
-                borderRadius: 25,
-              }}
-            />
+          <Image
+            source={images.profile}
+            style={{
+              ...iconSize(50),
+              borderRadius: 25,
+            }}
+          />
           {/* Name, Title */}
           <View
-            style={[styles.container, {
-              justifyContent: 'center',
-              marginLeft: SIZES.base,
-            }]}
-          >
+            style={[
+              styles.container,
+              {
+                justifyContent: 'center',
+                marginLeft: SIZES.base,
+              },
+            ]}>
             <Text
               style={{
                 ...FONTS.h3,
                 fontSize: 18,
-              }}
-            >
+              }}>
               {dummyData.course_details.instructor.name}
             </Text>
             <Text
               style={{
                 ...FONTS.body3,
                 color: COLORS.gray20,
-              }}
-            >
+              }}>
               {dummyData.course_details.instructor.title}
             </Text>
           </View>
@@ -86,23 +86,107 @@ const CourseChapters = ({title}) => {
             labelStyle={{
               ...FONTS.h3,
             }}
-            />
+          />
         </View>
       </View>
-    )
-  }
+    );
+  };
+  const renderChapter = () => {
+    return (
+      <View>
+        {dummyData.course_details.videos.map((item, index) => {
+          return (
+            <View
+              key={`video_${index}`}
+              style={{
+                alignItems: 'center',
+                height: 70,
+                backgroundColor: item.is_playing
+                  ? COLORS.additionalColor11
+                  : null,
+              }}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  paddingHorizontal: SIZES.padding,
+                  alignItems: 'center',
+                  height: 70,
+                }}>
+                {/* Icon */}
+                <Image
+                  source={
+                    item?.is_complete
+                      ? icons.completed
+                      : item?.is_playing
+                      ? icons.play_1
+                      : icons.lock
+                  }
+                  style={{
+                    ...iconSize(40),
+                  }}
+                />
+                {/* Title & Duration*/}
+                <View style={{flex: 1, marginLeft: SIZES.radius}}>
+                  <Text style={{...FONTS.h3}}>{item?.title}</Text>
+                  <Text style={{...FONTS.body4, color: COLORS.gray30}}>
+                    {item?.duration}
+                  </Text>
+                </View>
+                {/* Size & Status */}
+                <View style={{flexDirection: 'row'}}>
+                  {/* Size */}
+                  <Text
+                    style={{
+                      ...FONTS.body4,
+                      color: COLORS.gray30,
+                    }}>
+                    {item?.size}
+                  </Text>
+                  {/* Status */}
+                  <Image
+                    source={
+                      item?.is_downloaded ? icons.completed : icons.download
+                    }
+                    style={{
+                      ...iconSize(25),
+                      marginLeft: SIZES.base,
+                      tintColor: item?.is_lock ? COLORS.additionalColor4 : null,
+                    }}
+                  />
+                </View>
+              </View>
+              {item?.is_playing && (
+                <View
+                  style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    left: 0,
+                    height: 5,
+                    width: item?.progress,
+                    backgroundColor: COLORS.primary,
+                  }}
+                />
+              )}
+            </View>
+          );
+        })}
+      </View>
+    );
+  };
+
   return (
     <ScrollView>
       {/* Header */}
       {renderHeader()}
       {/* Line Divider */}
-      <LineDivider 
+      <LineDivider
         lineStyle={{
           height: 1,
-          marginVertical: SIZES.radius
+          marginVertical: SIZES.radius,
         }}
       />
       {/* Chapters */}
+      {renderChapter()}
       {/* Popular course */}
     </ScrollView>
   );
